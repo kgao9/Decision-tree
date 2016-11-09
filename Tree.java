c6839e1d0716f524cd0717fb07543be0b0e43144
Kenny Gao <kgao9@wisc.edu>
2016-11-09 10:00:36 -0600

    Ranges were too complex; find better examples

---
+++ Tree.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+
+/**
+* Best practice to use a lot of documentation
+* @author Kenny Gao
+* @version 11/8/2016
+*
+* @class cs 540
+*/
+
+public class Tree
+{
+    /**
+    * The purpose of this class is to get the best question
+    * @param questions - array list of questions
+    * @param examples - array list of cars
+    * @return the best question to ask
+    */
+
+    public static String bestQuestion(ArrayList <String> questions, ArrayList <Car> examples)
+    {
+        return "";
+    }
+
+    public static void main(String [] args)
+    {
+        System.out.println("cars loaded");
+    }
+}
